da1e210b96f8e44ec60893dee248c1b25b7bade6
argirowski <argirovski.g@gmail.com>
2021-04-09 12:20:24 +0200

    adding add and remove comments to the back end

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -173,4 +173,36 @@ router.post(
   }
 );
 
+// @route DELETE api/posts/comment:id/:comment_id
+// @desc Delete a comment
+// @access PRIVATE
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Pull out comment from post
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    //Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+    //Check if the user the deletes the comment is the user that created it
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not Authorized' });
+    }
+    //Get remove index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
