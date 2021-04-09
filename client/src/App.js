77c67860728feb4f2dc8edf83c0c28ebde528453
argirowski <argirovski.g@gmail.com>
2021-04-09 11:11:17 +0200

    finishing up with the back end and adding some react / front end stuff

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { Fragment, useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from '../src/components/layout/Navbar';
+import Landing from '../src/components/layout/Landing';
+import Alert from '../src/components/layout/Alert';
+import Login from '../src/components/auth/Login';
+import Register from '../src/components/auth/Register';
+import Dashboard from '../src/components/dashboard/Dashboard';
+import PrivateRoute from '../src/components/routing/PrivateRoute';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser);
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
